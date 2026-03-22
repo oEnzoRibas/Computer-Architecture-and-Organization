fbb8128e87e123909ec176a78236055196847039
Enzo Ribas <enzorochaleitedinizribas@gmail.com>
2026-03-22 18:22:46 -0300

    Organize materials: add old files and move slides
    Add and reorganize course materials: import numerous PDFs, DOCX, images and MIPS assembly examples into Old/2022 and Old/2024 directories, add Estudos Dirigidos PDFs, and add exam images. Rename/move several Slides and Exercícios files (e.g. Slides/2026 -> Slides or Old/2024 as appropriate) and include example MIPS asm files (ola_mundo.asm, mips1.asm) and related resources.

---
+++ Old/2022/Exércicios/ola_mundo.asm
@@ -0,0 +1,11 @@
+.data  # àreas do assembly = especificação de variaveis / lidar com os dados na mèmoria principal
+
+	msg:.asciiz "Olá, mundo!"  #asciiz = variavel referente a cadeia de caractéres
+
+
+.text
+	# Área para instruções do programa
+	li $v0,4 #Instrução para impressao de String
+	la $a0, msg #Indicar o endereço em que está a mensagwem
+	
+	syscall # imprime o que estiver dentro do registrador == faça!
